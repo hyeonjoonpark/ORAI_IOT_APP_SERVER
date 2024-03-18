da5a51679486c94fcf11dd2e1fae00e962515170
hyeonjoonpark <pjjoon1379@gmail.com>
2024-03-18 14:35:23 +0900

    SET: 프로젝트 세팅

---
+++ src/main/java/bsm/iot/orai/OraiApplication.java
@@ -0,0 +1,13 @@
+package bsm.iot.orai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OraiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OraiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/bsm/iot/orai/OraiApplicationTests.java
@@ -0,0 +1,13 @@
+package bsm.iot.orai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OraiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
